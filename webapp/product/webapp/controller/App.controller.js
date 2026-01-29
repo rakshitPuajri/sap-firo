0050952afe373db9463b2fc4019185223f4db92f
Rakshit  Pujari <pujari@threerun.de>
2026-01-29 10:29:01 +0100

    supplier app

---
+++ webapp/product/webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define(
+   ["sap/ui/core/mvc/Controller"],
+   function(Controller){
+      return Controller.extend("NorthWindApp.product.Controller",{
+         metadata:{
+            manifest:"json"
+         }
+      })
+   }
+     )
