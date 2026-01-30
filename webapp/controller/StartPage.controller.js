000c42ab6699afc21b796cce7b797e9fdaec0799
Rakshit  Pujari <pujari@threerun.de>
2026-01-30 14:44:19 +0100

    navigation to rakshit profile and links

--- webapp/controller/StartPage.controller.js
+++ webapp/controller/StartPage.controller.js
@@ -2,28 +2,41 @@ sap.ui.define([
    "sap/ui/core/mvc/Controller",
    "sap/m/MessageToast",
    "sap/ui/core/UIComponent"
-   
-], (Controller,MessageToast,UIComponent) => {
+
+], (Controller, MessageToast, UIComponent) => {
    "use strict";
 
-  return Controller.extend("ui5app.controller.StartPage", {
-      onProfile: function(evt){
-         MessageToast.show('Welcome to Rakshit Profile')
-      },
+   return Controller.extend("ui5app.controller.StartPage", {
       press() {
          // show a native JavaScript alert
-           MessageToast.show('Sales fulfilement app')
+         MessageToast.show('Sales fulfilement app')
       },
-      onInovicesApp : function(){
+      onInovicesApp: function () {
          MessageToast.show('pressed on the invoice');
-         console.log(  this.getRouter().navTo("InvoiceList"));
+         console.log(this.getRouter().navTo("InvoiceList"));
 
          this.getRouter().navTo("InvoiceList");
 
       },
+      onRakshitProfile: function () {
+         MessageToast.show('Welcome to My Profile');
+         this.getRouter().navTo("RakshitPujari")
+      },
+      onRakshitLinkedin: function () {
+         window.open(
+            "https://www.linkedin.com/in/rakshit-pujari-5b4109151/",
+            "_blank"
+         );
+      },
+      onBlog: function () {
+         window.open(
+            "https://rakshitpujari-blogs.vercel.app/",
+            "_blank"
+         );
+      },
       getRouter: function () {
-			return this.getOwnerComponent().getRouter();
-		}
+         return this.getOwnerComponent().getRouter();
+      }
    });
-    
+
 });
