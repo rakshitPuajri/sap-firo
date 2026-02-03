73baeb9b6ff6347d9d98fe2598ae9b16c93e92a4
Rakshit  Pujari <pujari@threerun.de>
2026-02-03 12:37:50 +0100

    Product app changes and filter

--- webapp/controller/ProductList.controller.js
+++ webapp/controller/ProductList.controller.js
@@ -2,20 +2,42 @@ sap.ui.define([
    "sap/ui/core/mvc/Controller",
    "sap/m/MessageToast",
    "sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast,JSONModel) {
+], function (Controller, MessageToast, JSONModel) {
    "use strict";
 
    return Controller.extend("ui5app.controller.ProductList", {
       onInit: function () {
-         console.log('started the prod list controller');
-         
-         var oModel = this.getView().getModel("ProductsModel");
-         console.log(oModel.getData());
+
+         var oModel = this.getOwnerComponent().getModel("ProductsModel");
+         console.log(this.getOwnerComponent().getModel("ProductsModel"));
+
+
       },
 
       onNavButtonPressed: function () {
          MessageToast.show('navigate back to start page')
          this.getOwnerComponent().getRouter().navTo("StartPage")
+      },
+      onFilterProducts: function (oEvent) {
+         const sQuery = oEvent.getParameter("newValue");
+         const oTable = this.byId("productsTable");
+         const oBinding = oTable.getBinding("items");
+
+         const aFilters = [];
+
+         if (sQuery) {
+            aFilters.push(
+               new sap.ui.model.Filter(
+                  "ProductName",
+                  sap.ui.model.FilterOperator.Contains,
+                  sQuery
+               )
+            );
+           
+         }
+         oBinding.filter(aFilters);
       }
+
+
    });
 });
