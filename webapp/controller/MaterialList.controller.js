403cfdf071163763e32936208edeb89b1f756a42
Rakshit  Pujari <pujari@threerun.de>
2026-02-12 12:21:35 +0100

    Added loading screen until the API call is sucessfull

--- webapp/controller/MaterialList.controller.js
+++ webapp/controller/MaterialList.controller.js
@@ -6,13 +6,31 @@ sap.ui.define(
         "use strict"
         return Controller.extend("ui5app.controller.MaterialList", {
             onInit: function () {
-                 var oModel = new sap.ui.model.json.JSONModel();
+                let oModel = new sap.ui.model.json.JSONModel();
+                let oUIModel = new sap.ui.model.json.JSONModel({
+                    isLoading: true
+                });
+                
                 // console.log(this.getOwnerComponent().getModel("MaterialModel"));
+                
+                let self = this;
+                
+                // Attach event handlers before loading data
+                oModel.attachRequestCompleted(function() {
+                    console.log("Materials loaded successfully");
+                    oUIModel.setProperty("/isLoading", false);
+                });
+                
+                oModel.attachRequestFailed(function(oEvent) {
+                    console.error("Error loading materials", oEvent);
+                    oUIModel.setProperty("/isLoading", false);
+                });
                
-                 oModel.loadData("https://supapp-3htg.onrender.com/api/v1/materials/getMaterials");
+                oModel.loadData("https://supapp-3htg.onrender.com/api/v1/materials/getMaterials");
 
                 this.getView().setModel(oModel, "materialsModel");
-                console.log(  this.getView().setModel(oModel, "materialsModel"));
+                this.getView().setModel(oUIModel, "uiModel");
+                console.log(this.getView().setModel(oModel, "materialsModel"));
                 
 
             },
