01d44b74984ba5422b4b6b5e3c977102de8ad659
Rakshit  Pujari <pujari@threerun.de>
2026-02-12 12:38:19 +0100

    Added subheader and footer to materials app tile on start page

--- webapp/controller/MaterialList.controller.js
+++ webapp/controller/MaterialList.controller.js
@@ -11,25 +11,28 @@ sap.ui.define(
                     isLoading: true
                 });
                 
-                // console.log(this.getOwnerComponent().getModel("MaterialModel"));
-                
                 let self = this;
+                
+                // Set models first
+                this.getView().setModel(oModel, "materialsModel");
+                this.getView().setModel(oUIModel, "uiModel");
                 
                 // Attach event handlers before loading data
                 oModel.attachRequestCompleted(function() {
                     console.log("Materials loaded successfully");
                     oUIModel.setProperty("/isLoading", false);
+                    oUIModel.refresh(true);
                 });
                 
                 oModel.attachRequestFailed(function(oEvent) {
                     console.error("Error loading materials", oEvent);
                     oUIModel.setProperty("/isLoading", false);
+                    oUIModel.refresh(true);
                 });
                
+                console.log("Starting to load materials, isLoading:", oUIModel.getProperty("/isLoading"));
                 oModel.loadData("https://supapp-3htg.onrender.com/api/v1/materials/getMaterials");
 
-                this.getView().setModel(oModel, "materialsModel");
-                this.getView().setModel(oUIModel, "uiModel");
                 console.log(this.getView().setModel(oModel, "materialsModel"));
                 
 
